33924cdfe69319a62b5a1274e68807deaa1786d3
Azamhonchic <109133231+Azamhonchic@users.noreply.github.com>
2022-09-05 20:22:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let Plus = document.querySelector("#Plus");
+let Minus = document.querySelector("#Minus");
+let count = document.querySelector("#Number");    
+let reload = document.querySelector('.reload')
+let random = document.querySelector('.random')
+let number = 1;
+
+Plus.onclick = function() {
+    if (number <= 99) {
+        number++;
+        count.innerHTML = number;
+    }
+};
+
+Minus.onclick = function() {
+    if (number >= 2) {
+        number--;
+        count.innerHTML = number;
+    }
+}
+
+        reload.onclick = function() {
+            (count > 0) 
+                count.innerHTML = 0
+            
+                count.innerHTML = 0
+            
+        }
+        random.onclick = function() {
+            (count > 0) 
+                count.innerHTML = Math.floor(Math.random() * 100)
+            
+                count.innerHTML = Math.floor(Math.random() * 100)
+            
+        }   
